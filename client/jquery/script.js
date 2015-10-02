d088d4a9f3b3b7777312b470095c77a88e35b3f6
Nora Top <top.nora@gmail.com>
2015-10-02 20:12:06 +0000

    post new entry to addressbook 1 ok

--- client/jquery/script.js
+++ client/jquery/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    $('div').click(function(){
+    $('#listEntriesButton').click(function(){
         var filter = '?filter={"order": "firstName ASC"}'
         $.ajax({
           method: "GET",
-          url: "https://loopback-workshop-noratop.c9.io/api/Entries"+filter,
+          url: "/api/Entries"+filter,
           dataType: "JSON"
         })
         .done(function( msg ) {
@@ -14,6 +14,40 @@ $(document).ready(function(){
             })
         });    
     });
+    
+    $('#addThisEntry').click(function(){
+        
+        
+        if (!$("input[id=firstName]").val()) {alert("Please enter a first name"); return;}
+        if (!$("input[id=lastName]").val()) {alert("Please enter a last name"); return;}
+
+        var data = {
+            "firstName" : $("input[id=firstName]").val(),
+            "lastName" : $("input[id=lastName]").val()
+        }
+        
+        if ($("input[id=birthday]").val()) {data.birthday = $("input[id=birthday]").val()}
+
+        console.log(data);
+        console.log(JSON.stringify(data));
+        
+        $.ajax({
+            type: "POST",
+            url: "api/AddressBooks/1/entries",
+            data: JSON.stringify(data),
+            contentType:"application/json",
+        })
+        .done(function( msg ) {
+            console.log( "Data Saved: " + msg );
+            // msg.forEach(function(entry){
+            //     console.log(entry);
+            //     $('#result').append("<li>"+entry.firstName+" "+entry.lastName+"</li>");
+            // })
+        });    
+    });    
+
+
+
     $('.button').hover(function(){
         $(this).toggleClass('back');
     });
