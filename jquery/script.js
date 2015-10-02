3748d4fbf4396665c8318aea16d795d0b3aff819
Nora Top <top.nora@gmail.com>
2015-10-02 15:45:01 +0000

    removed obsolete files

--- jquery/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('#button').click(function(){
-        var toAdd = "hello";
-        $('#result').append("<p>"+toAdd+"</p>");
-    });
-    $('#button').hover(function(){
-        $(this).toggleClass('back');
-    });
-});
