cd7ad30d72385a9aaf2c6966aee862c6675a125b
Nora Top <top.nora@gmail.com>
2015-10-02 02:26:35 +0000

    ready for review

--- client/jquery/script.js
+++ client/jquery/script.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
     $('#button').click(function(){
-        var toAdd = "hello";
-        $('#result').append("<p>"+toAdd+"</p>");
+        var filter = '?filter={"order": "firstName ASC"}'
+        $.ajax({
+          method: "GET",
+          url: "https://loopback-workshop-noratop.c9.io/api/Entries"+filter,
+          dataType: "JSON"
+        })
+        .done(function( msg ) {
+            //alert( "Data Saved: " + msg );
+            msg.forEach(function(entry){
+                console.log(entry);
+                $('#result').append("<li>"+entry.firstName+" "+entry.lastName+"</li>");
+            })
+        });    
     });
     $('#button').hover(function(){
         $(this).toggleClass('back');
