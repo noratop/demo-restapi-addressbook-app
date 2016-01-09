b4418ebd1af6f5548416387ff6dcae7b7b056ed3
Nora <top.nora@gmail.com>
2016-01-09 17:48:05 -0500

    added mysql credentials access

---
+++ server/datasources.local.js
@@ -0,0 +1,19 @@
+/**
+ * Created by noratop on 2016-01-09.
+ */
+var url = require('url');
+var mysql_url = process.env.CLEARDB_DATABASE_URL;
+var parsed_url = url.parse(mysql_url);
+var auth = parsed_url.auth.split(':');
+var username = auth[0];
+var password = auth[1];
+var database = parsed_url.pathname.substring(1);
+
+module.exports = {
+    addressBookDatabase: {
+        username: username,
+        password: password,
+        hostname: parsed_url.hostname,
+        database: database
+    }
+};
